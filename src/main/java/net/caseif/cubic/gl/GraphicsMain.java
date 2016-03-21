e28e4c6e55f4b33ea67463f63a4be9773270ec65
Max Roncace <me@caseif.net>
2016-03-21 07:57:57 -0400

    Fix key listener not accepting multiple inputs simultaneously

--- src/main/java/net/caseif/cubic/gl/GraphicsMain.java
+++ src/main/java/net/caseif/cubic/gl/GraphicsMain.java
@@ -25,11 +25,47 @@
 
 package net.caseif.cubic.gl;
 
-import static net.caseif.cubic.util.GLU.gluPerspective;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL20.*;
-import static org.lwjgl.opengl.GL30.glBindFragDataLocation;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.opengl.GL11.GL_COLOR_ARRAY;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.GL_FALSE;
+import static org.lwjgl.opengl.GL11.GL_LEQUAL;
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.GL_TRUE;
+import static org.lwjgl.opengl.GL11.GL_VERTEX_ARRAY;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glDepthFunc;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glEnableClientState;
+import static org.lwjgl.opengl.GL11.glFrustum;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glRotatef;
+import static org.lwjgl.opengl.GL11.glViewport;
+import static org.lwjgl.opengl.GL20.glGetUniformLocation;
+import static org.lwjgl.opengl.GL20.glUniformMatrix4fv;
+import static org.lwjgl.opengl.GL20.glUseProgram;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
 import net.caseif.cubic.Main;
@@ -39,11 +75,7 @@ import net.caseif.cubic.gl.render.ShaderHelper;
 import net.caseif.cubic.gl.render.SimpleWorldRenderer;
 import net.caseif.cubic.input.KeyListener;
 import net.caseif.cubic.math.matrix.Matrix4f;
-import net.caseif.cubic.util.GLU;
 import net.caseif.cubic.util.MatrixHelper;
-
-import com.sun.prism.ps.Shader;
-import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWKeyCallback;
 import org.lwjgl.glfw.GLFWVidMode;
@@ -53,8 +85,8 @@ import java.io.IOException;
 
 public class GraphicsMain implements Runnable {
 
-    private static final int WINDOW_WIDTH = 800;
-    private static final int WINDOW_HEIGHT = 800;
+    private static final int WINDOW_WIDTH = 600;
+    private static final int WINDOW_HEIGHT = 600;
     public static final Camera CAMERA = new Camera();
 
     private GLFWErrorCallback errorCallback = GLFWErrorCallback.createPrint(System.err);
--- src/main/java/net/caseif/cubic/input/KeyListener.java
+++ src/main/java/net/caseif/cubic/input/KeyListener.java
@@ -1,8 +1,39 @@
+/*
+ * GLExperiments
+ * Copyright (c) 2016, Maxim Roncace <me@caseif.net>
+ *
+ * The MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package net.caseif.cubic.input;
 
-import static java.awt.SystemColor.window;
 import static net.caseif.cubic.gl.render.Camera.MOVE_DISTANCE;
-import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_A;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_D;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_LEFT_SHIFT;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_S;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_SPACE;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_W;
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.glfwGetKey;
 
 import net.caseif.cubic.gl.GraphicsMain;
 
@@ -19,11 +50,13 @@ public class KeyListener {
             GraphicsMain.CAMERA.moveLeft(MOVE_DISTANCE);
         } else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
             GraphicsMain.CAMERA.moveRight(MOVE_DISTANCE);
-        } else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        }
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
             GraphicsMain.CAMERA.moveForward(MOVE_DISTANCE);
         } else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
             GraphicsMain.CAMERA.moveBackward(MOVE_DISTANCE);
-        } else if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+        }
+        if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
             GraphicsMain.CAMERA.moveUp(MOVE_DISTANCE);
         } else if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
             GraphicsMain.CAMERA.moveDown(MOVE_DISTANCE);
